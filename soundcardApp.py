e196bb26ce0d5e93cdd701c2bc6cbf1d874d85c2
Vilosidl <Tock2121@protonmail.com>
2025-07-18 02:49:24 +0000

    Scripts

---
+++ soundcardApp.py
@@ -0,0 +1,9 @@
+import socket
+
+def send_command(host, port, command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+        sock.sendall(command.encode('utf-8'))  # Отправляем команду
+        response = sock.recv(1024)  # Получаем ответ
+
+send_command("127.0.0.1", 65432, 'change')

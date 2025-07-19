59f41ea141cb8b8c206777316cba54316ea7bcd3
Vilosidl <Tock2121@protonmail.com>
2025-07-19 01:02:38 +0000

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 def send_command(host, port, command):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
-        sock.sendall(command.encode('utf-8'))  # Отправляем команду
-        response = sock.recv(1024)  # Получаем ответ
+        sock.sendall(command.encode('utf-8'))
+        response = sock.recv(1024)
 
 send_command("127.0.0.1", 65432, 'change')
